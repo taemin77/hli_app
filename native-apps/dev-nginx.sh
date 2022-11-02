20fa50ae0910403e281098cf4892a10c79a49021
taemi <taemi@DESKTOP-74H56GK>
2022-11-02 10:48:56 +0900

    전체 폴더 구조 변경

--- native-apps/dev-nginx.sh
+++ native-apps/dev-nginx.sh
@@ -1,34 +1,30 @@
-DEPLOY=$1
-DEPLOY_OPTION=$2
+PROFILE=$1
+DEPLOY=$2
+DEPLOY_OPTION=$3
 
 # helm패키지조회
 if [ ${DEPLOY_OPTION} == "helm-list" ]; then
-    helm list --kubeconfig ~/.kube/dev-config -n ${DEPLOY}
+    helm list --kubeconfig ~/.kube/${PROFILE}-config -n ${DEPLOY}
 fi
 
 # helm패키지배포
 if [ ${DEPLOY_OPTION} == "helm-deploy" ]; then
-    helm upgrade --install --kubeconfig ~/.kube/dev-config ${DEPLOY} ./helm-packages/${DEPLOY}/ -n ${DEPLOY} -f ./custom-yamls/${DEPLOY}/dev-values.yaml
+    helm upgrade --install --kubeconfig ~/.kube/${PROFILE}-config ${DEPLOY} ./helm-packages/${DEPLOY}/ -n ${DEPLOY} -f ./custom-yamls/${DEPLOY}/${PROFILE}-values.yaml
 fi
 
 # helm패키지삭제
 if [ ${DEPLOY_OPTION} == "helm-remove" ]; then
-    helm uninstall --kubeconfig ~/.kube/dev-config ${DEPLOY} -n ${DEPLOY}
+    helm uninstall --kubeconfig ~/.kube/${PROFILE}-config ${DEPLOY} -n ${DEPLOY}
 fi
 
 # k8s리소스조회
 if [ ${DEPLOY_OPTION} == "resource-list" ]; then
-    kubectl get -f ./custom-yamls/${DEPLOY}/dev-namespace.yaml --kubeconfig ~/.kube/dev-config
-
-    if [ ${DEPLOY} == "ingress-nginx" ] || [ ${DEPLOY} == "prometheus" ] || [ ${DEPLOY} == "loki-stack" ]; then
-        kubectl get -f ./custom-yamls/${DEPLOY}/dev-service.yaml --kubeconfig ~/.kube/dev-config
-    fi
+    kubectl get -f ./custom-yamls/${DEPLOY}/${PROFILE}-namespace.yaml --kubeconfig ~/.kube/${PROFILE}-config
+    kubectl get -f ./custom-yamls/${DEPLOY}/${PROFILE}-service.yaml --kubeconfig ~/.kube/${PROFILE}-config
 fi
+
 # k8s리소스배포
 if [ ${DEPLOY_OPTION} == "resource-deploy" ]; then
-    kubectl apply -f ./custom-yamls/${DEPLOY}/dev-namespace.yaml --kubeconfig ~/.kube/dev-config
-
-    if [ ${DEPLOY} == "ingress-nginx" ] || [ ${DEPLOY} == "prometheus" ] || [ ${DEPLOY} == "loki-stack" ]; then
-        kubectl apply -f ./custom-yamls/${DEPLOY}/dev-service.yaml --kubeconfig ~/.kube/dev-config
-    fi
+    kubectl apply -f ./custom-yamls/${DEPLOY}/${PROFILE}-namespace.yaml --kubeconfig ~/.kube/${PROFILE}-config
+    kubectl apply -f ./custom-yamls/${DEPLOY}/${PROFILE}-service.yaml --kubeconfig ~/.kube/${PROFILE}-config
 fi
